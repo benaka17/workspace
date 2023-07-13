cc7fcd728ef6197e93b4c03eae351a7ae72b4a39
benaka17 <benaka@bzz.ch>
2023-07-13 09:14:45 +0000

    cleaned up files

---
+++ src/main/java/ch/zli/m223/model/Booking.java
@@ -0,0 +1,84 @@
+package ch.zli.m223.model;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private long id;
+
+    @Column(nullable = false)
+    @NotNull
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @ManyToOne
+    private ApplicationUser applicationUser;
+
+    public Booking(LocalDate date, String status,
+        ApplicationUser applicationUser) {
+            this.date = date;
+            this.status = status;
+            this.applicationUser = applicationUser;
+    }
+
+    public Booking() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ApplicationUser getApplicationUser() {
+        return applicationUser;
+    }
+
+    public void setApplicationUser(ApplicationUser applicationUser) {
+        this.applicationUser = applicationUser;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+  
+
+}
